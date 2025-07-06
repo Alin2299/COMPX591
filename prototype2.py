282aa7c5db08242a181c2aa8ccb3b63a16ad4f55
Andrew Lin <Andrewlin125@gmail.com>
2025-07-06 22:03:38 +1200

    6/07/2025 PM Brought the interactive map code into prototype 2 from the sandbox, and arranged the UI into 2 columns

--- prototype2.py
+++ prototype2.py
@@ -7,16 +7,30 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import helper
+import matplotlib as plt
+import helper
+import json
+import streamlit_folium
+import folium
+import geopandas as gpd
+from shapely.geometry import Point
+from datetime import datetime, timedelta
 
+# Setup the UI and UI size, such as the title and columns
+st.set_page_config(layout="wide")
 st.title("Interactive Electricity Grid Model - Prototype 2")
+col1, col2 = st.columns([0.5, 0.5])
 
-# Load all data
+# Load all relevant data
 data_dir = "Data/"
 fleet_df = helper.load_file(f"{data_dir}Fleet-31Mar2025.csv")
-march_demand_df = helper.load_file(f"{data_dir}Demand_trends_202503.csv", 11)
+march_demand_df = helper.load_file(f"{data_dir}Demand_trends_zone_202503.csv", 11)
 march_generation_df = helper.load_file(f"{data_dir}202503_Generation_MD.csv")
+region_path = f"{data_dir}WGS84_GeoJSON_Zone.JSON"
 
-# Process/prepare the dataframe
+# Code for displaying the  electricity demand and supply graph
+
+# Process/prepare the generation dataframe
 # Clean the trading period columns, including converting units to MWh
 march_generation_df = march_generation_df.drop(columns=["TP49", "TP50"])
 tp_cols = [col for col in march_generation_df.columns if col.startswith("TP")]
@@ -73,25 +87,95 @@ desired_demand_row = wide_demand[wide_demand["Trading_Date"] == average_day]
 supply_values = desired_supply_row.drop(columns="Trading_Date").values.flatten()
 demand_values = desired_demand_row.drop(columns="Trading_Date").values.flatten()
 
+# Generate a list of plot time strings
+half_hour_times = [
+    (datetime.min + timedelta(minutes=i * 30)).strftime("%H:%M")
+    for i in range(48)
+]
+
 # Create a plot of the electricity demand vs supply
 chart_data = pd.DataFrame({
-    "Trading Period": list(range(1, 49)),
+    "Time": half_hour_times,
     "Demand (MWh)": demand_values,
     "Supply (MWh)": supply_values
 })
 
-st.subheader("Electricity Supply and Demand in New Zealand by Time")
-
 fig = px.line(
     chart_data, 
-    x="Trading Period", 
+    x="Time", 
     y=["Demand (MWh)", "Supply (MWh)"],
     range_y=[0, chart_data[["Demand (MWh)", "Supply (MWh)"]].max().max() * 1.1]
 )
 
 fig.update_layout(
-    xaxis_title="Trading Period",
-    yaxis_title="Electricity Amount (MWh)"
+    xaxis_title="Time",
+    yaxis_title="Electricity Amount (MWh)",
+    xaxis = dict(
+        dtick=6
+    )
 )
 
-st.plotly_chart(fig)
+with col1:
+    st.subheader("Electricity Supply and Demand in New Zealand by Time")
+    st.plotly_chart(fig)
+
+
+# Code for displaying the interactive map of New Zealand
+
+# Load the GeoJSON electricity grid regions data
+with open(region_path) as f:
+    region_gj = json.load(f)
+
+# Create a base map
+region_map = folium.Map(location=[-41, 174], zoom_start=5, height="700px")
+folium.GeoJson(
+    region_gj,
+    tooltip=folium.GeoJsonTooltip(
+        fields=["Region"],
+        aliases=["Region"],
+        localize=True
+    )
+).add_to(region_map)
+
+# Display the map in Streamlit
+with col2:
+    result = streamlit_folium.st_folium(region_map, width=700, height=700)
+
+# Get the fleet data and map that shows current territorial authorities
+fleet_df_2025 = helper.load_file(f"{data_dir}Fleet-31Mar2025.csv")
+ta_path =  f"{data_dir}territorial-authority-2025.json"
+ta_gdf = gpd.read_file(ta_path)
+region_gdf = gpd.read_file(region_path)
+
+# Prepare the data, including converting to the same CRS and ensuring that the geometry is valid
+ta_gdf = ta_gdf.to_crs(epsg=4326)
+region_gdf = region_gdf.to_crs(epsg=4326)
+region_gdf["geometry"] = region_gdf.buffer(0)
+
+# Spatial join the territorial authority and region data
+ta_with_regions = gpd.sjoin(ta_gdf, region_gdf, how="left", predicate="intersects")
+
+# Map the territorial authorities to the right regions, including handling uppercase words
+ta_region_map = dict(zip(ta_with_regions["TA2025_V_2"], ta_with_regions["Region"]))
+ta_region_map = {
+    k.upper(): v.upper()
+    for k, v in ta_region_map.items()
+}
+fleet_df_2025["REGION"] = fleet_df_2025["TLA"].map(ta_region_map)
+
+print(fleet_df_2025.head())
+
+# Handle interactivity on the map
+matched_region = None
+if result["last_clicked"] is not None:
+    clicked_point = Point(result["last_clicked"]["lng"], result["last_clicked"]["lat"])
+    match = region_gdf[region_gdf.geometry.contains(clicked_point)]
+    
+    if not match.empty:
+        matched_region = match["Region"].iloc[0]
+
+# Show the number of electric vehicles present in the currently selected region
+if matched_region is not None:
+    num_evs_region = ((fleet_df_2025["MOTIVE_POWER"] == "ELECTRIC") & (fleet_df_2025["REGION"] == matched_region.upper())).sum()
+    with col2:
+        st.write(f"There are {num_evs_region} electric vehicles in {matched_region}")
--- sandbox.py
+++ sandbox.py
@@ -12,61 +12,61 @@ import folium
 import geopandas as gpd
 from shapely.geometry import Point
 
-data_dir = "Data/"
-zone_path = f"{data_dir}WGS84_GeoJSON_Zone.JSON"
+# data_dir = "Data/"
+# region_path = f"{data_dir}WGS84_GeoJSON_Zone.JSON"
 
-# Load the GeoJSON electricity grid zones data
-with open(zone_path) as f:
-    zone_gj = json.load(f)
+# # Load the GeoJSON electricity grid regions data
+# with open(region_path) as f:
+#     region_gj = json.load(f)
 
-# Create a base map
-zone_map = folium.Map(location=[-41, 174], zoom_start=6, height=700, width=700)
-folium.GeoJson(
-    zone_gj,
-    tooltip=folium.GeoJsonTooltip(
-        fields=["Region"],
-        aliases=["Region"],
-        localize=True
-    )
-).add_to(zone_map)
+# # Create a base map
+# region_map = folium.Map(location=[-41, 174], zoom_start=6, height=700, width=700)
+# folium.GeoJson(
+#     region_gj,
+#     tooltip=folium.GeoJsonTooltip(
+#         fields=["Region"],
+#         aliases=["Region"],
+#         localize=True
+#     )
+# ).add_to(region_map)
 
-# Display the map in Streamlit
-result = streamlit_folium.st_folium(zone_map, width=700, height=700)
+# # Display the map in Streamlit
+# result = streamlit_folium.st_folium(region_map, width=700, height=700)
 
-# Get the fleet data and map showing current territorial authorities
-fleet_df_2025 = helper.load_file(f"{data_dir}Fleet-31Mar2025.csv")
-ta_path =  f"{data_dir}territorial-authority-2025.json"
-ta_gdf = gpd.read_file(ta_path)
-zone_gdf = gpd.read_file(zone_path)
+# # Get the fleet data and map that shows current territorial authorities
+# fleet_df_2025 = helper.load_file(f"{data_dir}Fleet-31Mar2025.csv")
+# ta_path =  f"{data_dir}territorial-authority-2025.json"
+# ta_gdf = gpd.read_file(ta_path)
+# region_gdf = gpd.read_file(region_path)
 
-# Prepare the data, including converting to the same CRS and ensuring that the geometry is valid
-ta_gdf = ta_gdf.to_crs(epsg=4326)
-zone_gdf = zone_gdf.to_crs(epsg=4326)
-zone_gdf["geometry"] = zone_gdf.buffer(0)
+# # Prepare the data, including converting to the same CRS and ensuring that the geometry is valid
+# ta_gdf = ta_gdf.to_crs(epsg=4326)
+# region_gdf = region_gdf.to_crs(epsg=4326)
+# region_gdf["geometry"] = region_gdf.buffer(0)
 
-# Spatial join the territorial authority and zone data
-ta_with_zones = gpd.sjoin(ta_gdf, zone_gdf, how="left", predicate="intersects")
+# # Spatial join the territorial authority and region data
+# ta_with_regions = gpd.sjoin(ta_gdf, region_gdf, how="left", predicate="intersects")
 
-# Map the territorial authorities to the right regions, including handling uppercase words
-ta_zone_map = dict(zip(ta_with_zones["TA2025_V_2"], ta_with_zones["Region"]))
-ta_zone_map = {
-    k.upper(): v.upper()
-    for k, v in ta_zone_map.items()
-}
-fleet_df_2025["ZONE"] = fleet_df_2025["TLA"].map(ta_zone_map)
+# # Map the territorial authorities to the right regions, including handling uppercase words
+# ta_region_map = dict(zip(ta_with_regions["TA2025_V_2"], ta_with_regions["Region"]))
+# ta_region_map = {
+#     k.upper(): v.upper()
+#     for k, v in ta_region_map.items()
+# }
+# fleet_df_2025["REGION"] = fleet_df_2025["TLA"].map(ta_region_map)
 
-print(fleet_df_2025.head())
+# print(fleet_df_2025.head())
 
-# Handle interactivity on the map
-matched_zone = None
-if result["last_clicked"] is not None:
-    clicked_point = Point(result["last_clicked"]["lng"], result["last_clicked"]["lat"])
-    match = zone_gdf[zone_gdf.geometry.contains(clicked_point)]
+# # Handle interactivity on the map
+# matched_region = None
+# if result["last_clicked"] is not None:
+#     clicked_point = Point(result["last_clicked"]["lng"], result["last_clicked"]["lat"])
+#     match = region_gdf[region_gdf.geometry.contains(clicked_point)]
     
-    if not match.empty:
-        matched_zone = match["Region"].iloc[0]
+#     if not match.empty:
+#         matched_region = match["Region"].iloc[0]
 
-# Show the number of electric vehicles present in the currently selected region
-if matched_zone is not None:
-    num_evs_region = ((fleet_df_2025["MOTIVE_POWER"] == "ELECTRIC") & (fleet_df_2025["ZONE"] == matched_zone.upper())).sum()
-    st.write(f"There are {num_evs_region} electric vehicles in {matched_zone}")
+# # Show the number of electric vehicles present in the currently selected region
+# if matched_region is not None:
+#     num_evs_region = ((fleet_df_2025["MOTIVE_POWER"] == "ELECTRIC") & (fleet_df_2025["REGION"] == matched_region.upper())).sum()
+#     st.write(f"There are {num_evs_region} electric vehicles in {matched_region}")
