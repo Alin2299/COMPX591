872e20b97df1cd34ac1fae509ef6d4c41786f2e6
Alin2299 <andrewlin125@gmail.com>
2025-08-25 22:08:28 +1200

    25/08/2025 PM Commited most recent changes before exploring a different plotting library

--- helper.py
+++ helper.py
@@ -1,12 +1,14 @@
 """
-Python file that contains helper code/functions for the project code
+Python file that contains helper code/functions for the project code; in particular, functions that help implement Streamlit caching properly
+
+Note the use of caching with some of the functions to help optimise performance
 """
 
 # Import the necessary libraries/packages
 import pandas as pd
 import streamlit as st
+import geopandas as gpd
 
-# Stops unnecessary refreshes
 @st.cache_resource
 def load_file(path, num_skip_rows=0): 
     """
@@ -19,3 +21,123 @@ def load_file(path, num_skip_rows=0):
     df = pd.read_csv(path, skiprows=num_skip_rows)
 
     return df
+
+
+def get_electric_mask(df):
+    """
+    Function that returns a mask for filtering out electric vs non-electric powered vehicles in the fleet dataframes
+
+    df: Dataframe to be processed
+
+    Returns: The appropriate mask
+    """
+    electric_mask = (df["MOTIVE_POWER"].isin(["ELECTRIC", "ELECTRIC [PETROL EXTENDED]", "ELECTRIC FUEL CELL HYDROGEN"])) | (df["MOTIVE_POWER"].str.contains("PLUGIN", na=False))
+    return electric_mask
+
+@st.cache_data
+def get_cleaned_fleet_df(fleet_path, ta_region_map):
+    """
+    Function that processes and maps the fleet dataframe to the regions so that caching works as expected 
+
+    fleet_path: The filepath to the fleet data file
+    ta_region_map: The GeoJSON file showing the territorial authorities in New Zealand
+
+    Returns: The processed dataframe
+
+    """
+
+    # Load and apply  mapping
+    raw_df = load_file(fleet_path)
+    raw_df["REGION"] = raw_df["TLA"].map(ta_region_map)
+
+    # Remove invalid and ambiguous fleet data
+    raw_df = raw_df[raw_df["REGION"].notna()].copy()
+    raw_df = raw_df[raw_df["MOTIVE_POWER"] != "OTHER"]
+
+    return raw_df
+
+@st.cache_data
+def build_region_fleet_summary(fleet_df):
+    """
+    Function that creates a summary dataframe of the New Zealand fleet composition and information, by region, for use in scenarios and interactivity
+
+    fleet_df: The dataframe describing the fleet
+
+    Returns: A dataframe representing the summarised information
+    """
+    # Get the list of regions that the vehicles could be associated with from the dataframe
+    vehicle_regions = fleet_df["REGION"].unique()
+
+    # For each region, create a row that contains information about the size and characteristics/composition of the vehicles in that region
+    rows_list = []
+
+    for region in vehicle_regions:
+        filtered_fleet_df = fleet_df[fleet_df["REGION"] == region]
+
+        # Filter to get different relevant values such as the number of total electric vehicles in the given region
+        electric_mask = get_electric_mask(filtered_fleet_df)
+        num_light_electric_vehicles = filtered_fleet_df[(electric_mask) & (filtered_fleet_df["GROSS_VEHICLE_MASS"] <= 3500)].shape[0]
+        num_heavy_electric_vehicles = filtered_fleet_df[(electric_mask) & (filtered_fleet_df["GROSS_VEHICLE_MASS"] > 3500)].shape[0]
+
+        num_light_combustion_vehicles = filtered_fleet_df[(~electric_mask) & (filtered_fleet_df["GROSS_VEHICLE_MASS"] <= 3500)].shape[0]
+        num_heavy_combustion_vehicles = filtered_fleet_df[(~electric_mask) & (filtered_fleet_df["GROSS_VEHICLE_MASS"] > 3500)].shape[0]
+
+        # Add to the appropriate temporary list
+        rows_list.append({
+            "Region": region,
+            "Light Electric Vehicle Count": num_light_electric_vehicles,
+            "Heavy Electric Vehicle Count": num_heavy_electric_vehicles,
+            "Light Combustion Vehicle Count": num_light_combustion_vehicles,
+            "Heavy Combustion Vehicle Count": num_heavy_combustion_vehicles
+        })
+    summary_df = pd.DataFrame(rows_list).set_index("Region")
+    totals_row = summary_df.sum()
+
+    summary_df.loc["NEW ZEALAND"] = totals_row
+
+    return summary_df
+
+@st.cache_data
+def get_avg_profiles(region, day_index, supply_df, demand_df):
+    """
+    Function that calculates the average supply and demand profile for a given region and weekday index
+
+    region: The region to be filtered on
+    day_index: The array index that corresponds to the user-selected day of the week to be profiled
+    supply_df: The processed electricity supply dataframe
+    demand_df: The processed electricity demand dataframe
+
+    Returns: The average electricity supply and demand profiles for the supplied region and weekday
+    """
+
+    if region != "New Zealand":
+        supply_df = supply_df[supply_df["Region"] == region]
+        demand_df = demand_df[demand_df["Region"] == region]
+
+    supply_df["Trading_Date"] = pd.to_datetime(supply_df["Trading_Date"])
+    demand_df["Trading_Date"] = pd.to_datetime(demand_df["Trading_Date"])
+
+    supply_avg = supply_df[supply_df["Trading_Date"].dt.dayofweek == day_index].select_dtypes(include="number").mean(axis=0)
+    demand_avg = demand_df[demand_df["Trading_Date"].dt.dayofweek == day_index].select_dtypes(include="number").mean(axis=0)
+
+    return demand_avg, supply_avg
+
+
+@st.cache_data
+def get_ta_region_map(ta_path, region_path):
+    """
+    Function that maps NZ territorial authorities to the given region data
+
+    ta_path: The file path to the territorial authority mapping data
+    region_path: The file path to the regions mapping data
+
+    Returns: A processed dictionary representing the final joined data/mapping
+    """
+    ta_gdf = gpd.read_file(ta_path).to_crs(epsg=4326)
+    region_gdf = gpd.read_file(region_path).to_crs(epsg=4326)
+    region_gdf["geometry"] = region_gdf.buffer(0)
+
+    ta_with_regions = gpd.sjoin(ta_gdf, region_gdf, how="left", predicate="intersects")
+
+    ta_region_map = dict(zip(ta_with_regions["TA2025_V_2"], ta_with_regions["Region"]))
+    return {k.upper(): v.upper() for k, v in ta_region_map.items()}
--- prototype2.py
+++ prototype2.py
@@ -8,7 +8,6 @@ import pandas as pd
 import plotly.express as px
 import helper
 import matplotlib as plt
-import helper
 import json
 import streamlit_folium
 import folium
@@ -16,6 +15,7 @@ import geopandas as gpd
 from shapely.geometry import Point
 from geopandas import GeoDataFrame
 from datetime import datetime, timedelta
+import numpy as np
 
 # Setup the UI and UI size, such as the title and columns
 st.set_page_config(layout="wide")
@@ -31,6 +31,8 @@ st.title("Interactive Electricity Grid Model - Prototype 2")
 col1, col2 = st.columns([0.5, 0.5])
 
 # Load all relevant data
+import time
+
 data_dir = "Data/"
 fleet_df = helper.load_file(f"{data_dir}Fleet-31Mar2025.csv")
 march_demand_df = helper.load_file(f"{data_dir}Demand_trends_zone_202503.csv", 11)
@@ -38,63 +40,9 @@ march_generation_df = helper.load_file(f"{data_dir}202503_Generation_MD.csv")
 region_path = f"{data_dir}WGS84_GeoJSON_Zone.JSON"
 network_mapping_df = helper.load_file(f"{data_dir}20250614_NetworkSupplyPointsTable.csv")
 
+
 # Variable for tracking which region has been selected 
 selected_region = "New Zealand"
-
-@st.cache_data
-def get_cleaned_fleet_df(fleet_path, ta_region_map):
-    raw_df = helper.load_file(fleet_path)
-    # ta_path =  f"{data_dir}territorial-authority-2025.json"
-    # ta_gdf = gpd.read_file(ta_path)
-
-    # # Prepare the data, including converting to the same CRS and ensuring that the geometry is valid
-    # ta_gdf = ta_gdf.to_crs(epsg=4326)
-    # region_gdf = region_gdf.to_crs(epsg=4326)
-    # region_gdf["geometry"] = region_gdf.buffer(0)
-
-    # # Spatial join the territorial authority and region data
-    # ta_with_regions = gpd.sjoin(ta_gdf, region_gdf, how="left", predicate="intersects")
-
-    # # Map the territorial authorities to the right regions, including handling uppercase words
-    # ta_region_map = dict(zip(ta_with_regions["TA2025_V_2"], ta_with_regions["Region"]))
-    # ta_region_map = {
-    #     k.upper(): v.upper()
-    #     for k, v in ta_region_map.items()
-    # }
-    raw_df["REGION"] = raw_df["TLA"].map(ta_region_map)
-
-    raw_df = raw_df[raw_df["REGION"].notna()].copy()
-    raw_df = raw_df[raw_df["MOTIVE_POWER"] != "OTHER"]
-
-    return raw_df
-
-@st.cache_data
-def build_region_fleet_summary(fleet_df):
-    # Get the list of regions that the vehicles could be associated with from the dataframe
-    vehicle_regions = fleet_df_2025["REGION"].unique()
-
-    # For each region, create a row that contains information about the size and characteristics/composition of the vehicles in that region
-    rows_list = []
-
-    for region in vehicle_regions:
-        filtered_fleet_df = fleet_df_2025[fleet_df_2025["REGION"] == region]
-
-        num_light_vehicles = filtered_fleet_df[filtered_fleet_df["GROSS_VEHICLE_MASS"] <= 3500].shape[0]
-        num_heavy_vehicles = filtered_fleet_df[filtered_fleet_df["GROSS_VEHICLE_MASS"] > 3500].shape[0]
-
-        electric_mask = (filtered_fleet_df["MOTIVE_POWER"].isin(["ELECTRIC", "ELECTRIC [PETROL EXTENDED]", "ELECTRIC FUEL CELL HYDROGEN"])) | (filtered_fleet_df["MOTIVE_POWER"].str.contains("PLUGIN", na=False))
-        num_electric_vehicles = filtered_fleet_df[electric_mask].shape[0]
-        num_combustion_vehicles = filtered_fleet_df[~electric_mask].shape[0]
-
-        rows_list.append({
-            "Region": region,
-            "Light Vehicle Count": num_light_vehicles,
-            "Heavy Vehicle Count": num_heavy_vehicles,
-            "Electric Vehicle Count": num_electric_vehicles,
-            "Combustion Vehicle Count": num_combustion_vehicles
-        })
-
-    return pd.DataFrame(rows_list).set_index("Region")
 
 
 # Code for displaying the interactive map of New Zealand
@@ -139,51 +87,19 @@ region_gdf = region_gdf.to_crs(epsg=4326)
 region_gdf["geometry"] = region_gdf.buffer(0)
 
 # Spatial join the territorial authority and region data
-ta_with_regions = gpd.sjoin(ta_gdf, region_gdf, how="left", predicate="intersects")
+# ta_with_regions = gpd.sjoin(ta_gdf, region_gdf, how="left", predicate="intersects")
 
 # Map the territorial authorities to the right regions, including handling uppercase words
-ta_region_map = dict(zip(ta_with_regions["TA2025_V_2"], ta_with_regions["Region"]))
-ta_region_map = {
-    k.upper(): v.upper()
-    for k, v in ta_region_map.items()
-}
+# ta_region_map = dict(zip(ta_with_regions["TA2025_V_2"], ta_with_regions["Region"]))
+# ta_region_map = {
+#     k.upper(): v.upper()
+#     for k, v in ta_region_map.items()
+# }
+ta_region_map = helper.get_ta_region_map(ta_path, region_path)
 
-fleet_df_2025 = get_cleaned_fleet_df(f"{data_dir}Fleet-31Mar2025.csv", ta_region_map)
-# fleet_df_2025["REGION"] = fleet_df_2025["TLA"].map(ta_region_map)
+fleet_df_2025 = helper.get_cleaned_fleet_df(f"{data_dir}Fleet-31Mar2025.csv", ta_region_map)
 
-# fleet_df_2025 = fleet_df_2025[fleet_df_2025["REGION"].notna()].copy()
-# fleet_df_2025 = fleet_df_2025[fleet_df_2025["MOTIVE_POWER"] != "OTHER"]
-
-# Create a new simplified dataframe for storing information about the fleet, its composition, and for handling the different scenarios
-
-# # Get the list of regions that the vehicles could be associated with from the dataframe
-# vehicle_regions = fleet_df_2025["REGION"].unique()
-
-# # For each region, create a row that contains information about the size and characteristics/composition of the vehicles in that region
-# rows_list = []
-
-# for region in vehicle_regions:
-#     filtered_fleet_df = fleet_df_2025[fleet_df_2025["REGION"] == region]
-
-#     num_light_vehicles = filtered_fleet_df[filtered_fleet_df["GROSS_VEHICLE_MASS"] <= 3500].shape[0]
-#     num_heavy_vehicles = filtered_fleet_df[filtered_fleet_df["GROSS_VEHICLE_MASS"] > 3500].shape[0]
-
-#     electric_mask = (filtered_fleet_df["MOTIVE_POWER"].isin(["ELECTRIC", "ELECTRIC [PETROL EXTENDED]", "ELECTRIC FUEL CELL HYDROGEN"])) | (filtered_fleet_df["MOTIVE_POWER"].str.contains("PLUGIN", na=False))
-#     num_electric_vehicles = filtered_fleet_df[electric_mask].shape[0]
-#     num_combustion_vehicles = filtered_fleet_df[~electric_mask].shape[0]
-
-#     rows_list.append({
-#         "Region": region,
-#         "Light Vehicle Count": num_light_vehicles,
-#         "Heavy Vehicle Count": num_heavy_vehicles,
-#         "Electric Vehicle Count": num_electric_vehicles,
-#         "Combustion Vehicle Count": num_combustion_vehicles
-#     })
-
-# region_fleet_df = pd.DataFrame(rows_list)
-# region_fleet_df.set_index("Region", inplace=True)
-
-region_fleet_df = build_region_fleet_summary(fleet_df_2025)
+region_fleet_df = helper.build_region_fleet_summary(fleet_df_2025)
 
 # Handle interactivity on the map
 if result["last_clicked"] is not None:
@@ -193,22 +109,42 @@ if result["last_clicked"] is not None:
     if not match.empty:
         selected_region = match["Region"].iloc[0]
 
-# Show the number of electric vehicles present in the currently selected region
+
 # if selected_region != "New Zealand":
-#     num_evs_region = ((fleet_df_2025["MOTIVE_POWER"] == "ELECTRIC") & (fleet_df_2025["REGION"] == selected_region.upper())).sum()
+light_evs_region = region_fleet_df.loc[selected_region.upper(), "Light Electric Vehicle Count"]
+heavy_evs_region = region_fleet_df.loc[selected_region.upper(), "Heavy Electric Vehicle Count"]
+num_evs_region = light_evs_region + heavy_evs_region
 # else:
-#     num_evs_region = (fleet_df_2025["MOTIVE_POWER"] == "ELECTRIC").sum()
-
-# with col2:
-#     st.write(f"There are {num_evs_region} electric vehicles in {selected_region}")
-
-if selected_region != "New Zealand":
-    num_evs_region = region_fleet_df.loc[selected_region.upper(), "Electric Vehicle Count"]
-else:
-    num_evs_region = region_fleet_df["Electric Vehicle Count"].sum()
+#     num_evs_region = region_fleet_df["Light Electric Vehicle Count"].sum() + region_fleet_df["Heavy Electric Vehicle Count"].sum()
 
 with col2:
     st.write(f"There are {num_evs_region} electric vehicles in {selected_region}")
+
+    light_ev_df = fleet_df_2025[helper.get_electric_mask(fleet_df_2025) & 
+                        (fleet_df_2025["GROSS_VEHICLE_MASS"] < 3500)]
+    
+    heavy_ev_df = fleet_df_2025[helper.get_electric_mask(fleet_df_2025) & 
+                            (fleet_df_2025["GROSS_VEHICLE_MASS"] > 3500)]
+    
+    heavy_ev_df["MAKE_MODEL"] = heavy_ev_df["MAKE"] + " " + heavy_ev_df["MODEL"]
+    light_ev_df["MAKE_MODEL"] = light_ev_df["MAKE"] + " " + light_ev_df["MODEL"]
+
+    most_common_heavy_ev = heavy_ev_df["MAKE_MODEL"].mode()[0]
+    year_common_heavy_ev = heavy_ev_df[
+        heavy_ev_df["MAKE_MODEL"] == most_common_heavy_ev
+    ]["VEHICLE_YEAR"].mode()[0]
+
+    st.write(f"The most common heavy EV is the {year_common_heavy_ev} {most_common_heavy_ev}")
+
+
+    most_common_light_ev = light_ev_df["MAKE_MODEL"].mode()[0]
+    year_common_light_ev = light_ev_df[
+        light_ev_df["MAKE_MODEL"] == most_common_light_ev
+    ]["VEHICLE_YEAR"].mode()[0]
+
+    st.write(f"The most common light EV is the {year_common_light_ev} {most_common_light_ev}")
+
+
 
 
 # Code for displaying the  electricity demand and supply graph
@@ -312,24 +248,45 @@ with col1:
     day_options = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"]
     selected_day = st.selectbox(label="Day selector", options=day_options)
 
+# supply_values = supply_by_date_period[supply_by_date_period["Trading_Date"].dt.dayofweek == day_options.index(selected_day)].select_dtypes(include="number").mean(axis=0)
+# demand_values = wide_demand[wide_demand["Trading_Date"].dt.dayofweek == day_options.index(selected_day)].select_dtypes(include="number").mean(axis=0)
 
-# Find the rows that match the desired user choice for what data to show
-# desired_supply_row = supply_by_date_period[supply_by_date_period["Trading_Date"] == average_day]
-# desired_demand_row = wide_demand[wide_demand["Trading_Date"] == average_day]
-
-# Extract the values ready for plotting
-# supply_values = desired_supply_row.drop(columns="Trading_Date").values.flatten()
-# demand_values = desired_demand_row.drop(columns="Trading_Date").values.flatten()
-
-supply_values = supply_by_date_period[supply_by_date_period["Trading_Date"].dt.dayofweek == day_options.index(selected_day)].select_dtypes(include="number").mean(axis=0)
-demand_values = wide_demand[wide_demand["Trading_Date"].dt.dayofweek == day_options.index(selected_day)].select_dtypes(include="number").mean(axis=0)
-
+demand_values, supply_values = helper.get_avg_profiles(
+    selected_region,
+    day_options.index(selected_day),
+    supply_by_date_period,
+    wide_demand
+)
 
 # Generate a list of plot time strings
 half_hour_times = [
     (datetime.min + timedelta(minutes=i * 30)).strftime("%H:%M")
     for i in range(48)
 ]
+
+# if "fig" not in st.session_state:
+#     # Create a plot of the electricity demand vs supply
+#     chart_data = pd.DataFrame({
+#         "Time": half_hour_times,
+#         "Demand (MWh)": demand_values.values,
+#         "Supply (MWh)": supply_values.values
+#     })
+
+#     st.session_state["fig"] = px.line(
+#         chart_data, 
+#         x="Time", 
+#         y=["Demand (MWh)", "Supply (MWh)"],
+#         range_y=[0, chart_data[["Demand (MWh)", "Supply (MWh)"]].max().max() * 1.1]
+#     )
+
+#     st.session_state["fig"].update_layout(
+#         xaxis_title="Time",
+#         yaxis_title="Electricity Amount (MWh)",
+#         xaxis = dict(
+#             dtick=6
+#         ),
+#         height=500
+#     )
 
 # Create a plot of the electricity demand vs supply
 chart_data = pd.DataFrame({
@@ -338,23 +295,78 @@ chart_data = pd.DataFrame({
     "Supply (MWh)": supply_values.values
 })
 
-fig = px.line(
-    chart_data, 
-    x="Time", 
-    y=["Demand (MWh)", "Supply (MWh)"],
-    range_y=[0, chart_data[["Demand (MWh)", "Supply (MWh)"]].max().max() * 1.1]
-)
+# fig = px.line(
+#     chart_data, 
+#     x="Time", 
+#     y=["Demand (MWh)", "Supply (MWh)"],
+#     range_y=[0, chart_data[["Demand (MWh)", "Supply (MWh)"]].max().max() * 1.1]
+# )
 
-fig.update_layout(
-    xaxis_title="Time",
-    yaxis_title="Electricity Amount (MWh)",
-    xaxis = dict(
-        dtick=6
-    ),
-    height=500
-)
+# fig.update_layout(
+#     xaxis_title="Time",
+#     yaxis_title="Electricity Amount (MWh)",
+#     xaxis = dict(
+#         dtick=6
+#     ),
+#     height=500
+# )
 
+filtered_region_fleet = region_fleet_df.loc[selected_region.upper()]
+current_light_ev_share = (filtered_region_fleet["Light Electric Vehicle Count"] / (filtered_region_fleet["Light Electric Vehicle Count"] + filtered_region_fleet["Light Combustion Vehicle Count"])) * 100
+current_heavy_ev_share = (filtered_region_fleet["Heavy Electric Vehicle Count"] / (filtered_region_fleet["Heavy Electric Vehicle Count"] + filtered_region_fleet["Heavy Combustion Vehicle Count"])) * 100
 
 with col1:
     st.subheader(f"Average Electricity Supply and Demand in by Time for {selected_region}")
+    # st.plotly_chart(fig)
+    # st.plotly_chart(st.session_state["fig"])
+
+    target_light_ev_pct = st.slider("Light EV uptake (%)", value=current_light_ev_share, min_value=0.00, max_value=100.00)
+    target_heavy_ev_pct = st.slider("Heavy EV uptake (%)", value=current_heavy_ev_share, min_value=0.00, max_value=100.00)
+
+    needed_light_ev = (target_light_ev_pct / 100) * (filtered_region_fleet["Light Electric Vehicle Count"] + filtered_region_fleet["Light Combustion Vehicle Count"]) - filtered_region_fleet["Light Electric Vehicle Count"]
+    needed_heavy_ev = (target_heavy_ev_pct / 100) * (filtered_region_fleet["Heavy Electric Vehicle Count"] + filtered_region_fleet["Heavy Combustion Vehicle Count"]) - filtered_region_fleet["Heavy Electric Vehicle Count"]
+
+    # TODO REMOVE PLACEHOLDER VALUES 
+    light_ev_efficiency = 0.18
+    heavy_ev_efficiency = 0.80
+
+    daily_km_light_ev = 30
+    daily_km_heavy_ev = 300
+
+    kWh_day_light = daily_km_light_ev * light_ev_efficiency
+    kWh_day_heavy = daily_km_heavy_ev * heavy_ev_efficiency
+
+    extra_kWh_day = needed_light_ev * kWh_day_light + needed_heavy_ev * kWh_day_heavy
+
+    profile = np.ones(48) / 48
+    extra_MWh_per_slot = (extra_kWh_day / 1000) * profile
+
+    new_demand_values = demand_values.values + extra_MWh_per_slot
+    chart_data = pd.DataFrame({
+        "Time": half_hour_times,
+        "Demand (MWh)": new_demand_values,
+        "Supply (MWh)": supply_values.values
+    })
+    fig = px.line(chart_data, x="Time", y=["Demand (MWh)", "Supply (MWh)"])
+
+    fig.update_layout(
+            xaxis_title="Time",
+            yaxis_title="Electricity Amount (MWh)",
+            xaxis = dict(
+                dtick=6
+            ),
+            height=500
+        )
+
     st.plotly_chart(fig)
+
+    # fig = st.session_state["fig"]
+
+    # fig.data[0].y = new_demand_values
+    # fig.data[1].y = supply_values.values
+    # fig.update_yaxes(range=[0, 1.1 * max(max(new_demand_values), max(supply_values.values))])
+
+    # with col1:
+    #     st.plotly_chart(st.session_state["fig"], key=f"chart|{selected_region}|{selected_day}")
+
+
